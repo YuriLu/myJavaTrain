a566632f240fb0ec8dbaa965362b9fc95a7fcae9
Yuri Lukashevich <yurikluk@gmail.com>
2019-05-20 18:04:07 +0300

    Add files via upload

---
+++ lesson02/task01/Lesson2Main.java
@@ -0,0 +1,15 @@
+package lesson2;
+
+public class Lesson2Main {
+    public static void main(String[] args) {
+        DataTypesTester dataTypesTester =new DataTypesTester();
+        dataTypesTester.testByte();
+        dataTypesTester.testShort();
+        dataTypesTester.testInt();
+        dataTypesTester.testLong();
+        dataTypesTester.testChar();
+        dataTypesTester.testFloat();
+        dataTypesTester.testDouble();
+        dataTypesTester.testBoolean();
+    }
+}
