c63b7e1e6a8b785acb914e4109044d0ae55f4b1d
Yuri Lukashevich <yurikluk@gmail.com>
2019-05-27 14:19:12 +0300

    Update Lesson2Main.java
    Удалил абсолютно не нужное создание объекта класса DataTypesTester.
    Статические методы/свойства классов это такие методы/свойства, к которым можно обратиться не создавая объект данного класса.

--- lesson02/task01/Lesson2Main.java
+++ lesson02/task01/Lesson2Main.java
@@ -2,14 +2,13 @@ package lesson2;
 
 public class Lesson2Main {
     public static void main(String[] args) {
-        DataTypesTester dataTypesTester =new DataTypesTester();
-        dataTypesTester.testByte();
-        dataTypesTester.testShort();
-        dataTypesTester.testInt();
-        dataTypesTester.testLong();
-        dataTypesTester.testChar();
-        dataTypesTester.testFloat();
-        dataTypesTester.testDouble();
-        dataTypesTester.testBoolean();
+        DataTypesTester.testByte();
+        DataTypesTester.testShort();
+        DataTypesTester.testInt();
+        DataTypesTester.testLong();
+        DataTypesTester.testChar();
+        DataTypesTester.testFloat();
+        DataTypesTester.testDouble();
+        DataTypesTester.testBoolean();
     }
 }
