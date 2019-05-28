dc31e99323fdcd5208e2213be94e34e4899994e5
Yuri Lukashevich <yurikluk@gmail.com>
2019-05-28 07:50:56 +0300

    Delete DigitLogic.java

--- lesson04/task05/DigitLogic.java
+++
@@ -1,7 +0,0 @@
-package lesson04.task05;
-
-public class DigitLogic {
-    public static int getDigit(int number, int digitPosition) {
-        return (int) ((number / Math.pow(10, digitPosition - 1)) % 10);
-    }
-}
