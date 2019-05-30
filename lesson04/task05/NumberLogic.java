24a4005565e51ff14dcf788cbe3e7a632b4ae4be
Yuri Lukashevich <yurikluk@gmail.com>
2019-05-30 15:46:21 +0300

    update package name to correct and simplifying the code

--- lesson04/task05/NumberLogic.java
+++ lesson04/task05/NumberLogic.java
@@ -1,4 +1,4 @@
-package by.epam.training.yurilukashevich.lesson04.task05;
+package by.epam.javatraining.yurilukashevich.lesson04.task05;
 
 public class NumberLogic {
 
@@ -8,31 +8,14 @@ public class NumberLogic {
         int multiplier = 1000000;
 
         int digitSeven = number % 10 * multiplier;
-        number /= 10;
-        multiplier /= 10;
+        int digitSix = (number /= 10) % 10 * (multiplier /= 10);
+        int digitFive = (number /= 10) % 10 * (multiplier /= 10);
+        int digitFour = (number /= 10) % 10 * (multiplier /= 10);
+        int digitThree = (number /= 10) % 10 * (multiplier /= 10);
+        int digitTwo = (number /= 10) % 10 * (multiplier /= 10);
+        int digitOne = (number / 10) % 10 * multiplier / 10;
 
-        int digitSix = number % 10 * multiplier;
-        number /= 10;
-        multiplier /= 10;
-
-        int digitFive = number % 10 * multiplier;
-        number /= 10;
-        multiplier /= 10;
-
-        int digitFour = number % 10 * multiplier;
-        number /= 10;
-        multiplier /= 10;
-
-        int digitThree = number % 10 * multiplier;
-        number /= 10;
-        multiplier /= 10;
-
-        int digitTwo = number % 10 * multiplier;
-        number /= 10;
-        multiplier /= 10;
-
-        int digitOne = number % 10 * multiplier;
-
-        return digitSeven + digitSix + digitFive + digitFour + digitThree + digitTwo + digitOne;
+        return digitSeven + digitSix + digitFive
+                + digitFour + digitThree + digitTwo + digitOne;
     }
 }
